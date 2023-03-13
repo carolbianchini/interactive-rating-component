ade7fdec0f6533d68f36e57fdad745abde0b576e
carrottown <anabianchini@outlook.com.br>
2023-03-12 21:26:11 -0300

    Atualizações Finais

--- main.js
+++
@@ -1,8 +0,0 @@
-const classifica = document.querySelectorAll([data-classifica])
-
-classifica.forEach( (elemento) => {
-    elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento)
-    })
-})
-
